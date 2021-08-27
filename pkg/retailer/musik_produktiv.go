0bac9f43185fdc0c1e65fcc98978a9ec0949a7fa
Christian Mehring <cmeh@mailbox.org>
2021-08-27 12:15:07 +0200

    Add support for manufacturer names with spaces

--- pkg/retailer/musik_produktiv.go
+++ pkg/retailer/musik_produktiv.go
@@ -14,9 +14,10 @@ type MusikProduktiv struct {
 	http interface {
 		Get(url string) (*http.Response, error)
 	}
+	manufacturers []string
 }
 
-func (m MusikProduktiv) LoadProducts(category string) (ProductResponse, error) {
+func (m *MusikProduktiv) LoadProducts(category string) (ProductResponse, error) {
 	resp, err := m.http.Get(fmt.Sprintf("https://www.musik-produktiv.de/%s", category))
 	if err != nil {
 		return ProductResponse{}, fmt.Errorf("could not fetch products from musik-produktiv.de: %w", err)
@@ -27,13 +28,17 @@ func (m MusikProduktiv) LoadProducts(category string) (ProductResponse, error) {
 	if err != nil {
 		return ProductResponse{}, fmt.Errorf("could not create goquery document from reader: %w", err)
 	}
-
 	categoryName := doc.Find("div.list_title h1").Text()
 
-	nodes := doc.Find("ul.artgrid li")
-	instruments := make([]products.Product, len(nodes.Nodes))
+	manufacturerNodes := doc.Find(".mp-filtermenu ul").First().Find("li span")
+	m.manufacturers = make([]string, len(manufacturerNodes.Nodes))
+	manufacturerNodes.Each(func(i int, s *goquery.Selection) {
+		m.manufacturers[i] = s.Text()
+	})
 
-	nodes.Each(func(i int, s *goquery.Selection) {
+	instrumentNodes := doc.Find("ul.artgrid li")
+	instruments := make([]products.Product, len(instrumentNodes.Nodes))
+	instrumentNodes.Each(func(i int, s *goquery.Selection) {
 		p, err := m.parseProduct(s)
 		if err != nil {
 			return
@@ -46,7 +51,7 @@ func (m MusikProduktiv) LoadProducts(category string) (ProductResponse, error) {
 	return ProductResponse{Products: instruments}, nil
 }
 
-func (m MusikProduktiv) parseProduct(s *goquery.Selection) (products.Product, error) {
+func (m *MusikProduktiv) parseProduct(s *goquery.Selection) (products.Product, error) {
 	manufacturer, model := m.parseProductName(s.Find("b").First().Text())
 	price, err := m.parsePrice(s.Find("i").Text())
 	if err != nil {
@@ -64,13 +69,18 @@ func (m MusikProduktiv) parseProduct(s *goquery.Selection) (products.Product, er
 	}, nil
 }
 
-func (m MusikProduktiv) parseProductName(productName string) (manufacturer, model string) {
-	parts := strings.Split(productName, " ")
+func (m *MusikProduktiv) parseProductName(productName string) (manufacturer, model string) {
+	for _, man := range m.manufacturers {
+		if strings.HasPrefix(productName, man) {
+			return man, strings.TrimPrefix(productName, man+" ")
+		}
+	}
 
+	parts := strings.Split(productName, " ")
 	return parts[0], strings.TrimPrefix(productName, parts[0]+" ")
 }
 
-func (m MusikProduktiv) parsePrice(price string) (float64, error) {
+func (m *MusikProduktiv) parsePrice(price string) (float64, error) {
 	re := regexp.MustCompile("[^0-9]")
 	p := re.ReplaceAllString(price, "")
 
--- pkg/retailer/musik_produktiv_test.go
+++ pkg/retailer/musik_produktiv_test.go
@@ -6,19 +6,32 @@ import (
 )
 
 func TestMusikProduktiv_LoadProducts(t *testing.T) {
-	mp := MusikProduktiv{newTestHTTPClientForFixture("musikproduktiv_guitar_eight_strings.html")}
+	t.Run("parse product from product page", func(t *testing.T) {
+		mp := MusikProduktiv{http: newTestHTTPClientForFixture("musikproduktiv_guitars_eight_strings.html")}
 
-	response, err := mp.LoadProducts("e-gitarre-linkshaender")
-	assert.NoError(t, err)
+		response, err := mp.LoadProducts("e-gitarre-linkshaender")
+		assert.NoError(t, err)
 
-	assert.Len(t, response.Products, 1)
-	assert.Equal(t, "Musik Produktiv", response.Products[0].Retailer)
-	assert.Equal(t, "Schecter", response.Products[0].Manufacturer)
-	assert.Equal(t, "C-8 Deluxe LH SBK", response.Products[0].Model)
-	assert.Equal(t, "E-Gitarre (Linkshänder), 8-saitig", response.Products[0].Category)
-	assert.Equal(t, false, response.Products[0].IsAvailable)
-	assert.Equal(t, "", response.Products[0].AvailabilityInfo)
-	assert.Equal(t, float64(599), response.Products[0].Price)
-	assert.Equal(t, "https://www.musik-produktiv.de/schecter-c-8-deluxe-lh-sbk.html", response.Products[0].ProductURL)
-	assert.Equal(t, "https://sc1.musik-produktiv.com/pic-010125643l/schecter-c-8-deluxe-lh-sbk.jpg", response.Products[0].ThumbnailURL)
+		assert.Len(t, response.Products, 1)
+		assert.Equal(t, "Musik Produktiv", response.Products[0].Retailer)
+		assert.Equal(t, "Schecter", response.Products[0].Manufacturer)
+		assert.Equal(t, "C-8 Deluxe LH SBK", response.Products[0].Model)
+		assert.Equal(t, "E-Gitarre (Linkshänder), 8-saitig", response.Products[0].Category)
+		assert.Equal(t, false, response.Products[0].IsAvailable)
+		assert.Equal(t, "", response.Products[0].AvailabilityInfo)
+		assert.Equal(t, float64(599), response.Products[0].Price)
+		assert.Equal(t, "https://www.musik-produktiv.de/schecter-c-8-deluxe-lh-sbk.html", response.Products[0].ProductURL)
+		assert.Equal(t, "https://sc1.musik-produktiv.com/pic-010125643l/schecter-c-8-deluxe-lh-sbk.jpg", response.Products[0].ThumbnailURL)
+	})
+
+	t.Run("parse model and manufacturer titles when manufacturer name contains spaces", func(t *testing.T) {
+		mp := MusikProduktiv{http: newTestHTTPClientForFixture("musikproduktiv_guitars.html")}
+
+		response, err := mp.LoadProducts("e-gitarre-linkshaender")
+		assert.NoError(t, err)
+
+		assert.Len(t, response.Products, 20)
+		assert.Equal(t, "Gretsch Guitars", response.Products[14].Manufacturer)
+		assert.Equal(t, "G5230LH Electromatic LH Jet FT ASLV", response.Products[14].Model)
+	})
 }
