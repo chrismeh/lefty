4438b64a1195df434cc9d38d3b4b83c474dfed0b
Christian Mehring <cmeh@mailbox.org>
2021-09-03 18:06:21 +0200

    Use snake case when marshalling to JSON

--- pkg/products/products.go
+++ pkg/products/products.go
@@ -11,17 +11,17 @@ type Store interface {
 }
 
 type Product struct {
-	Retailer         string
-	Manufacturer     string
-	Model            string
-	Category         string
-	IsAvailable      bool
-	AvailabilityInfo string
-	Price            float64
-	ProductURL       string
-	ThumbnailURL     string
-	CreatedAt        time.Time
-	UpdatedAt        time.Time
+	Retailer         string    `json:"retailer"`
+	Manufacturer     string    `json:"manufacturer"`
+	Model            string    `json:"model"`
+	Category         string    `json:"category"`
+	IsAvailable      bool      `json:"is_available"`
+	AvailabilityInfo string    `json:"availability_info"`
+	Price            float64   `json:"price"`
+	ProductURL       string    `json:"product_url"`
+	ThumbnailURL     string    `json:"thumbnail_url"`
+	CreatedAt        time.Time `json:"created_at"`
+	UpdatedAt        time.Time `json:"updated_at"`
 }
 
 type Filter struct {
