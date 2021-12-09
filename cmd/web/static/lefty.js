a5dabe6116f04d4f741a09ad8d8618e225c86826
Christian Mehring <cmeh@mailbox.org>
2021-12-09 17:00:12 +0100

    Display a message when there are no products

--- cmd/web/static/lefty.js
+++ cmd/web/static/lefty.js
@@ -7,6 +7,5 @@ var app = new Vue({
         const response = await fetch("/api/products");
         const json = await response.json();
         this.products = json.data;
-        console.log(this.products);
     }
 })
